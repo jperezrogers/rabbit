7c4266f3b7a4815a9bf9aa5e9a37fcac4ac22820
Joe Perez-Rogers <jperezrogers@gmail.com>
2016-03-13 22:06:08 -0400

    changing some <- to = in module class

--- R/module-class.R
+++ R/module-class.R
@@ -24,26 +24,26 @@ Module <- R6Class("Module",
   public = list(
     
     # public members
-    label <- NA, # the name of the Module
+    label = NA, # the name of the Module
     
     # public methods
     addTask = function(){ # a function to create a new Task and add it to the Module (add or addTask?)
-      
+      invisible(self)
     },
     deleteTask = function(){ # a funtion to delete a Task from a Module (delete or deleteTask?)
-      
+      invisible(self)
     },
     run = function(){ # a function to execute a Task
-      
+      invisible(self)
     },
     activate = function(){ # a function to turn on a task or tasks
-      
+      invisible(self)
     },
     deactivate = function(){ # a function to turn off a task or tasks
-      
+      invisible(self)
     },
     summary = function(){ # a function that will print out a summary of the Tasks in the Module
-      
+      invisible(self)
     }
 
   ),
@@ -51,13 +51,13 @@ Module <- R6Class("Module",
   private = list(
     
     # private members
-    class <- NA, # the child class of the Module
-    tasks <- NA, # a list of Task objects
-    active <- NA, # a Boolean vector of length = number of Tasks
+    class = NA, # the child class of the Module
+    tasks = NA, # a list of Task objects
+    active = NA, # a Boolean vector of length = number of Tasks
     
     # private methods
     addTaskMember = function(){ # a function to add a Task as a member of the Module object
-      
+      invisible(self)
     }
     
   ),
