8f1a3748d298714e37329adbf309bd09fd7bdfb5
Joe Perez-Rogers <jperezrogers@gmail.com>
2016-03-31 17:33:35 -0400

    adding validateDatatype, man page, and unit tests

---
+++ R/module-validateDatatype-method.R
@@ -0,0 +1,31 @@
+#' Validate that input datatype conforms to required standards of the Task class
+#' 
+#' More detailed explaination here
+#' 
+#' @param label One or more of \code{microarray}, \code{count}, \code{rpkm}
+#' 
+#' @return Function will execute silently if no errors are detected in \code{label}
+#' 
+#' @examples
+#' validateDatatype("microarray")
+#' validateDatatype(c("microarray","count"))
+#' validateDatatype(c("microarray","count","rpkm"))
+
+validateDatatype <- function(datatype=NULL){
+  
+  # check if datatype is NULL
+  if(is.null(datatype)){
+    stop("The datatype of a Task cannot be NULL")
+  }
+  
+  # check that datatype is a character
+  if(!all(sapply(datatype,is.character))){
+    stop("The datatype of a Task must be a character vector")
+  }
+  
+  # check that datatype is in c("microarray","count","rpkm")
+  if(!all(datatype%in%c("microarray","count","rpkm"))){
+    stop("The datatype of a Task must be one or more of 'microarray', 'count', 'rpkm'")
+  }
+  
+}
