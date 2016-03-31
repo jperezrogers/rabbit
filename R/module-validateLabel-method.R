e16d0db3acce832e683418028d23d78a2d8ffb37
Joe Perez-Rogers <jperezrogers@gmail.com>
2016-03-31 16:53:59 -0400

    adding validateLabel script plus man page and assoc unit tests

---
+++ R/module-validateLabel-method.R
@@ -0,0 +1,29 @@
+#' Validate that input label conforms to required standards of the Task class
+#' 
+#' More detailed explaination here
+#' 
+#' @param label A character string
+#' 
+#' @return Function will execute silently if no errors are detected in \code{label}
+#' 
+#' @examples
+#' validateLabel("label")
+
+validateLabel <- function(label=NULL){
+  
+  # check if label is NULL
+  if(is.null(label)){
+    stop("The label of a Task cannot be NULL")
+  }
+  
+  # check that label is of length 1
+  if(length(label)>1){
+    stop("The label of a Task must be a character vector of length 1")
+  }
+  
+  # check that the label is of class character
+  if(!is.character(label)){
+    stop("The label of a Task must be of class character")
+  }
+  
+}
