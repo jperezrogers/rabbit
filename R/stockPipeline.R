c5839099234b9ebdf7deb5293f7145facba5b9d4
Joe Perez-Rogers <jperezrogers@gmail.com>
2016-10-21 13:41:18 -0400

    temporarily removing stock pipeline

--- R/stockPipeline.R
+++
@@ -1,9 +0,0 @@
-#' Stock implementation of a biomarker discovery pipeline
-#' 
-#' A more detailed description will eventually go here...
-#' 
-#' @format a \code{Pipeline} object containing 4 modules and 15 total tasks.
-#' 
-#' @examples
-#' summary(stockPipeline, level="structure")
-"stockPipeline"
